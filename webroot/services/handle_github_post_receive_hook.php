6efc7de9cf32b66e7e0c49cd5e37367b36942556
Martin Bless <m.bless@gmx.de>
2014-05-27 18:23:38 +0200

    [TASK] Little changes to Github hook receiver

--- webroot/services/handle_github_post_receive_hook.php
+++ webroot/services/handle_github_post_receive_hook.php
@@ -1,17 +1,11 @@
 <?php
-/**
-* Created by JetBrains PhpStorm.
-* User: marble
-* Date: 19.03.13
-* Time: 13:28
-* To change this template use File | Settings | File Templates.
-*/
 
 error_reporting(E_ALL);
 ini_set('error_reporting', E_ALL);
 
 $NL = "\n";
-$f2path = '/home/mbless/public_html/services/temp.txt';
+$f2path = '/home/mbless/public_html/services/log-of-notifications.txt';
+$f3path = '/home/mbless/public_html/services/args-of-last-request.txt';
 
 $mapping = array(
 	'https://github.com/froemken/typo3-extbase-guide' => 'http://docs.typo3.org/~mbless/github.com/froemken/typo3-extbase-guide.git.make/request_rebuild.php',
@@ -23,18 +17,6 @@ echo $f2path;
 echo '<br>' . $NL;
 $f2 = fopen($f2path, 'a');
 if ($f2) {
-	if (0) {
-		$args = print_r($_POST, TRUE);
-		echo htmlspecialchars($args);
-		fwrite($f2, $args);
-		fwrite($f2, $NL);
-	}
-	if (0) {
-		$args = print_r($_GET, TRUE);
-		echo htmlspecialchars($args);
-		fwrite($f2, $args);
-		fwrite($f2, $NL);
-	}
 	if (1) {
 		$github = FALSE;
 		$github = @json_decode($_POST['payload'], TRUE);
@@ -62,6 +44,29 @@ if ($f2) {
 	fclose($f2);
 } else {
 	echo "Could not open file.";
+}
+
+$f3 = fopen($f3path, 'w');
+if ($f3) {
+	if (1) {
+		$args = print_r($_POST, TRUE);
+		echo htmlspecialchars($args);
+		fwrite($f3, $args);
+		fwrite($f3, $NL);
+	}
+	if (1) {
+		$args = print_r($_GET, TRUE);
+		echo htmlspecialchars($args);
+		fwrite($f3, $args);
+		fwrite($f3, $NL);
+	}
+	if (1) {
+		$args = print_r($github, TRUE);
+		echo htmlspecialchars($args);
+		fwrite($f3, $args);
+		fwrite($f3, $NL);
+	}
+	fclose($f3);
 }
 
 ?>
