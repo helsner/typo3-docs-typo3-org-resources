4cec645c113c37a96bcb9aab7ded49e339f3f495
Martin Bless <m.bless@gmx.de>
2016-02-22 17:20:23 +0100

    [TASK] Update systemextensions.js for 7.6

--- webroot/typo3cms/extensions/systemextensions.js
+++ webroot/typo3cms/extensions/systemextensions.js
@@ -1,27 +1,27 @@
 // manually maintained list of system extensions
-// mb, 2015-01-22, 2015-11-22
+// mb, 2015-11-22, 2016-01-27
 
 // merge list 'systemExtensionList' into list 'extensionList'
 
 var systemExtensionList = [
-	{"key":"core","latest":"latest","versions":["latest"]},
-	{"key":"css_styled_content","latest":"latest","versions":["4.7","6.2","latest","latest"]},
-	{"key":"dbal","latest":"latest","versions":["1.0.0","6.2","latest","latest"]},
-	{"key":"documentation","latest":"latest","versions":["6.2","latest","latest"]},
-	{"key":"felogin","latest":"latest","versions":["4.7","6.2","latest","latest"]},
-	{"key":"fluid_styled_content","latest":"latest","versions":["latest","latest"]},
-	{"key":"form","latest":"latest","versions":["4.7","6.2","latest","latest"]},
-	{"key":"indexed_search","latest":"latest","versions":["6.2","latest","latest"]},
-	{"key":"linkvalidator","latest":"latest","versions":["6.2","latest","latest"]},
-	{"key":"openid","latest":"latest","versions":["4.7","6.2","latest","latest"]},
-	{"key":"recycler","latest":"latest","versions":["4.7","6.2","latest","latest"]},
-	{"key":"rsaauth","latest":"latest","versions":["4.7","6.2","latest","latest"]},
-	{"key":"rtehtmlarea","latest":"latest","versions":["4.7","6.2","latest","latest"]},
-	{"key":"saltedpasswords","latest":"latest","versions":["4.7","6.2","latest","latest"]},
-	{"key":"scheduler","latest":"latest","versions":["4.7","6.2","latest","latest"]},
-	{"key":"sys_action","latest":"latest","versions":["4.7","6.2","latest","latest"]},
-	{"key":"taskcenter","latest":"latest","versions":["4.7","6.2","latest","latest"]},
-	{"key":"workspaces","latest":"latest","versions":["6.2","latest","latest"]}
+	{"key":"core","latest":"latest","versions":["7.6","latest"]},
+	{"key":"css_styled_content","latest":"latest","versions":["4.7","6.2","7.6","latest"]},
+	{"key":"dbal","latest":"latest","versions":["1.0.0","6.2","7.6","latest"]},
+	{"key":"documentation","latest":"latest","versions":["6.2","7.6","latest"]},
+	{"key":"felogin","latest":"latest","versions":["4.7","6.2","7.6","latest"]},
+	{"key":"fluid_styled_content","latest":"latest","versions":["7.6","latest"]},
+	{"key":"form","latest":"latest","versions":["4.7","6.2","7.6","latest"]},
+	{"key":"indexed_search","latest":"latest","versions":["6.2","7.6","latest"]},
+	{"key":"linkvalidator","latest":"latest","versions":["6.2","7.6","latest"]},
+	{"key":"openid","latest":"latest","versions":["4.7","6.2","7.6","latest"]},
+	{"key":"recycler","latest":"latest","versions":["4.7","6.2","7.6","latest"]},
+	{"key":"rsaauth","latest":"latest","versions":["4.7","6.2","7.6","latest"]},
+	{"key":"rtehtmlarea","latest":"latest","versions":["4.7","6.2","7.6","latest"]},
+	{"key":"saltedpasswords","latest":"latest","versions":["4.7","6.2","7.6","latest"]},
+	{"key":"scheduler","latest":"latest","versions":["4.7","6.2","7.6","latest"]},
+	{"key":"sys_action","latest":"latest","versions":["4.7","6.2","7.6","latest"]},
+	{"key":"taskcenter","latest":"latest","versions":["4.7","6.2","7.6","latest"]},
+	{"key":"workspaces","latest":"latest","versions":["6.2","7.6","latest"]}
 ];
 
 // var extensionList needs to exist at this point
